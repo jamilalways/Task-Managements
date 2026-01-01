544eeb59bca43c0bc06b7bef261f7e38c8e4824e
Jamil Ahmed <jamilahmedbd60@gmail.com>
2026-01-01 11:04:14 +0600

    admin feature

--- tasks/views.py
+++ tasks/views.py
@@ -18,3 +18,5 @@ def show_specific_task(request,id):
     return HttpResponse("This is specific task page")
 def dashboard(request,id):
     return HttpResponse("This is Dashboard")
+def admin(request):
+    return HttpResponse("This is admin feature implementation")
