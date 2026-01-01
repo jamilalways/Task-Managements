3024bf36c6a150973424d8763c96ec9861ccdb4f
Jamil Ahmed <jamilahmedbd60@gmail.com>
2026-01-01 10:40:20 +0600

    dashboard

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from tasks.views import show_task,show_specific_task
+from tasks.views import show_task,show_specific_task,dashboard
 urlpatterns = [
     path("show-task/",show_task),
     path("show_task/<int:id>/",show_specific_task),
+    path("das/",dashboard)
 ]
